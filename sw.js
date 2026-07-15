404086df22a2705b5ecdc981e7214e07ff7bbb50
feliipeac17 <feliipeac17@gmail.com>
2026-07-15 16:06:27 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'ae-diag-cache-v1';
+const assets = [
+  'index.html',
+  'manifest.json',
+  'logo2.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cachedResponse => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
